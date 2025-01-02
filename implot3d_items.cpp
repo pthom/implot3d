331d5b2a6e6073ba93bb0a4bd6eb3b106286e2bd
Pascal Thomet <pthomet@gmail.com>
2025-01-02 14:43:12 +0100

    [Bundle] PlotSurface - add a version whose API is easier to port

--- implot3d_items.cpp
+++ implot3d_items.cpp
@@ -1367,6 +1367,33 @@ IMPLOT3D_TMP void PlotSurface(const char* label_id, const T* xs, const T* ys, co
 CALL_INSTANTIATE_FOR_NUMERIC_TYPES()
 #undef INSTANTIATE_MACRO
 
+#ifdef IMGUI_BUNDLE_PYTHON_API
+// A version of PlotSurface whose API is easier to port to Python
+//    (params xs_count, ys_count and zs_count are removed in the Python API, but are used in the bindings code generation)
+IMPLOT3D_TMP void PlotSurface(
+        const char* label_id,
+        const T* xs, int xs_count,
+        const T* ys, int ys_count,
+        const T* zs, int zs_count,
+        int x_count, int y_count,
+        double scale_min, double scale_max,
+        ImPlot3DSurfaceFlags flags,
+        int offset,
+        int stride)
+{
+    IM_ASSERT(xs_count == x_count * y_count);
+    IM_ASSERT(ys_count == x_count * y_count);
+    IM_ASSERT(zs_count == x_count * y_count);
+    PlotSurface(label_id, xs, ys, zs, x_count, y_count, scale_min, scale_max, flags, offset, stride);
+}
+#define INSTANTIATE_MACRO(T) \
+    template IMPLOT3D_API void PlotSurface<T>(const char* label_id, const T* xs, int xs_count, const T* ys, int ys_count, const T* zs, int zs_count, int x_count, int y_count, double scale_min, double scale_max, ImPlot3DSurfaceFlags flags, int offset, int stride);
+CALL_INSTANTIATE_FOR_NUMERIC_TYPES()
+#undef INSTANTIATE_MACRO
+
+#endif // IMGUI_BUNDLE_PYTHON_API
+
+
 //-----------------------------------------------------------------------------
 // [SECTION] PlotMesh
 //-----------------------------------------------------------------------------
