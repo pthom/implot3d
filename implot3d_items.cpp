60a083f83601c50e75b5ec08aeb4cf44994b44e6
Pascal Thomet <pthomet@gmail.com>
2026-04-17 19:22:21 +0200

    [Bundle] fixup Adapt API for PlotMesh / need to add a struct Mesh: use new API internally

--- implot3d_items.cpp
+++ implot3d_items.cpp
@@ -1583,7 +1583,16 @@ void PlotMesh(const char* label_id, const ImPlot3DPoint* vtx, const unsigned int
 #ifdef IMGUI_BUNDLE_PYTHON_API
 void PlotMesh(const char* label_id, const Mesh& mesh, const ImPlot3DSpec& spec)
 {
-    PlotMesh(label_id, mesh.Points.data(), mesh.Idx.data(), (int)mesh.Points.size(), (int)mesh.Idx.size(), spec);
+    if (mesh.Points.empty())
+        return;
+
+    ImPlot3DSpec s = spec;
+    s.Offset = 0;
+    s.Stride = (int)sizeof(ImPlot3DPoint);  // Trick to avoid splitting the data: use Stride
+    const ImPlot3DPoint* vtx = mesh.Points.data();
+    int vtx_count = (int)mesh.Points.size();
+
+    PlotMesh<double>(label_id, &vtx->x, &vtx->y, &vtx->z, mesh.Idx.data(), vtx_count, (int)mesh.Idx.size(), s);
 }
 #endif // IMGUI_BUNDLE_PYTHON_API
 
